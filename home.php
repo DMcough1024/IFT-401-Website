3dd882f5cb286a564429920d4a649136352d8ef4
DMcough1024 <DMcough1024@gmail.com>
2023-10-23 03:09:20 -0500

    Checking different db

--- home.php
+++ home.php
@@ -70,8 +70,10 @@
     <?php
     echo $_SESSION['user_perm'];
     if ($_SESSION['user_perm'] === 'admin') {
+        echo "<br>Hello Admin";
         $client_id = $_SESSION['client_id'];
-        $sql = "SELECT * FROM users WHERE client_id='" . $client_id . "'";
+        echo "<br>Client ID: " . $client_id;
+        $sql = "SELECT * FROM app_users.users WHERE client_id='" . $client_id . "'";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             echo "<h3>List of Users with Client ID " . $client_id . "</h3>";
