e03879c947d96644b5d2bbd2ddb752ac982caf41
DMcough1024 <DMcough1024@gmail.com>
2023-10-23 02:21:54 -0500

    Quick fix to logout

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    echo "Thank you for using our website " . $_SESSION['username'] . "!";
+    echo "Thank you for using our website " . $_SESSION['user_name'] . "!";
     session_unset();
     session_destroy();
     echo "<br><a href='index.html'>Go back to the login page</a>";
